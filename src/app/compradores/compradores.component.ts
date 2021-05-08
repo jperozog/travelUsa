ba3f20ee75e16306fd41ab5a0e6f289653861096
jperozog <perozo64@gmail.com>
2021-05-08 08:32:42 -0400

    algunos cambios finales

--- src/app/compradores/compradores.component.ts
+++ src/app/compradores/compradores.component.ts
@@ -8,9 +8,84 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 })
 export class CompradoresComponent implements OnInit {
   faStar = faStar;
+  tituloSeleccionado:string=""
+  contenidoSeleccionado:string=""
+  general: any = [
+    {
+      id: 1,
+      pregunta: "¿Cómo es el proceso de compra?",
+      contenido: "1.	Llenas el formulario, 2.	Recibes una cotización a tu correo o whatsapp, 3.	Haces el pago con Pago Seguro Paypal, 4.	Nosotros encontramos un BRINKER, que compre en USA por ti, 5.	Recibes tu producto en las puertas de tu casa"
+    },
+    {
+      id: 2,
+      pregunta: "¿Qué pasa si no encuentran lo que pedí? ",
+      contenido: "Te devolvemos el 100% de tu dinero"
+    },
+    {
+      id: 3,
+      pregunta: "¿Qué pasa si se equivocan con mi pedido?",
+      contenido: "Tienes 24 horas después de haber recibido tu pedido para contactarte con nosotros, recogemos el producto en tu hogar y te devolvemos el 100% de tu dinero"
+    },
+    {
+      id: 4,
+      pregunta: "¿Qué sucede tras pagar mi pedido?",
+      contenido: "Retenemos tu dinero hasta que confirmes de haberlo recibido en óptimas condiciones. Ya cuando hayas confirmado le pagaremos al BRINKER el costo del producto más su comisión por traerlo "
+    },
+    {
+      id: 5,
+      pregunta: "¿Cómo creo un pedido?",
+      contenido: "1.	Llena el formulario, 2.	Recibe el mail o whatsapp cotizándote, 3.	Paga por vía Paypal"
+    },
+    {
+      id: 6,
+      pregunta: "¿Dónde recibo mi pedido?",
+      contenido: "Te lo enviamos a la dirección que tu prefieras"
+    },
+    {
+      id: 7,
+      pregunta: "¿Por qué es el BRINKER quien compra mi artículo?",
+      contenido: "Porque así el BRINKER puede tener la certeza del equipaje que lleva consigo"
+    },
+    {
+      id: 8,
+      pregunta: "¿Qué sucede si mi pedido llega roto o incompleto?",
+      contenido: "Como comprador, estás cubierto por la garantía BRINKER, tu dinero no se le será entregado al comprador hasta que hayas confirmado la entrega de tu pedido. Tienes derecho a aceptar o no el artículo si el estado del artículo no es el acordado (color, cantidad, artículo incorrecto o si está dañado o roto). Asegúrate de inspeccionar tu pedido antes de confirmar la entrega. Los compradores tienen un periodo de 24 horas para reportar un pedido como dañado o incompleto. Recomendamos a los viajeros contratar seguro de viaje que cubra daños por pérdida de equipaje, etc. Si tu producto está dañado, roto o no es correcto, lo mejor es informarle a BRINKER inmediatamente. En algunos casos el BRINKER podrá traer un reemplazo. Caso contrario, la única solución es cancelar el pedido y te devolvemos al 100% tu dinero"
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  seleccionarPregunta(id:number){
+
+
+    
+    this.general.forEach((e:any) => {
+        if (e["id"] == id) {
+          this.tituloSeleccionado = e["pregunta"]
+          this.contenidoSeleccionado = e["contenido"]
+
+          let selected = document.getElementById(e["id"].toString());
+
+          let a = e["id"] + 100;
+
+          let selected2 = document.getElementById(a.toString());
+
+          selected?.setAttribute("style", "padding-left:1%; padding-right:1%;    box-shadow: rgba(22, 27, 29, 0.25) 0px 4px 16px;border-radius:5px; margin-top:1%;");
+          selected2?.setAttribute("style", "color:#01A0E2");
+        }else{
+          let selected = document.getElementById(e["id"].toString());
+
+          let a = e["id"] + 100;
+
+          let selected2 = document.getElementById(a.toString());
+
+          selected?.setAttribute("style", "padding-left:0; padding-right:0;    box-shadow: none;border-radius:0 px;margin-top:0 %; ");
+          selected2?.setAttribute("style", "color:gray");
+        }
+    });
+    
   }
 
 }
--- src/app/viajeros/viajeros.component.ts
+++ src/app/viajeros/viajeros.component.ts
@@ -8,9 +8,75 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 })
 export class ViajerosComponent implements OnInit {
   faStar = faStar;
+  tituloSeleccionado:string=""
+  contenidoSeleccionado:string=""
+  general: any = [
+    {
+      id: 1,
+      pregunta: "¿Cómo se garantiza mi pago?",
+      contenido: "Tu pago está asegurado y garantizado por Grabr. Los compradores pagan por adelantado y no pueden cancelar una vez pagan."
+    },
+    {
+      id: 2,
+      pregunta: "¿Por qué compro yo el producto? ",
+      contenido: "Compras el producto para saber qué es lo que llevas y ser responsable del contenido."
+    },
+    {
+      id: 3,
+      pregunta: "¿Dónde entrego el producto?",
+      contenido: "Al aceptar comprar un producto se te hará saber donde entregarlo, la mayoría de las veces es en las de algún courrier cerca a tu domicilio"
+    },
+    {
+      id: 4,
+      pregunta: "¿Puedo retirar mi oferta de entrega?",
+      contenido: "Si no puedes entregar el artículo todavía puedes cancelar la oferta pero es posible que esto afecte negativamente a las puntuaciones de tu perfil. Recuerda contactarnos para que sepamos que no vas a poder enviar el artículo y que cancelarás el pedido.  "
+    },
+    {
+      id: 5,
+      pregunta: "¿Cómo recibo el pago??",
+      contenido: "El pago se te hará 24 horas después de que el comprador haya recibido su producto en tu cuenta Paypal que te tienes que crear para recibir tu dinero"
+    },
+    {
+      id: 6,
+      pregunta: "El objeto no cabe en mi maleta",
+      contenido: "Si el objeto no cabe en tu maleta, puedes sacarlo de su envoltorio original. Asegúrate de contactarnos para notificarnos y a su vez nosotros le notifiquemos al comprador de que su compra llegará sin envoltorio"
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  
+  seleccionarPregunta(id:number){
+
+
+    
+    this.general.forEach((e:any) => {
+        if (e["id"] == id) {
+          this.tituloSeleccionado = e["pregunta"]
+          this.contenidoSeleccionado = e["contenido"]
+
+          let selected = document.getElementById(e["id"].toString());
+
+          let a = e["id"] + 100;
+
+          let selected2 = document.getElementById(a.toString());
+
+          selected?.setAttribute("style", "padding-left:1%; padding-right:1%;    box-shadow: rgba(22, 27, 29, 0.25) 0px 4px 16px;border-radius:5px; margin-top:1%;");
+          selected2?.setAttribute("style", "color:#01A0E2");
+        }else{
+          let selected = document.getElementById(e["id"].toString());
+
+          let a = e["id"] + 100;
+
+          let selected2 = document.getElementById(a.toString());
+
+          selected?.setAttribute("style", "padding-left:0; padding-right:0;    box-shadow: none;border-radius:0 px;margin-top:0 %; ");
+          selected2?.setAttribute("style", "color:gray");
+        }
+    });
+    
   }
 
 }
